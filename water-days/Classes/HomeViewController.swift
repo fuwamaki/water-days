efb7c387a14bc8f44351e1d542baf1d7ccf66930
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-03 16:04:21 +0900

    add stay water animation

--- water-days/Classes/HomeViewController.swift
+++ water-days/Classes/HomeViewController.swift
@@ -11,16 +11,25 @@ import BAFluidView
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet private var backgroundView: UIView!
+    @IBOutlet private weak var percentLabel: UILabel!
+
+    private var percent: NSNumber = 0.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let fluidView: BAFluidView = BAFluidView(frame: view.frame)
-        fluidView.fill(to: 1.0)
+        percent = 0.4
+        percentLabel.text = "40%"
+        let fluidView: BAFluidView = BAFluidView(frame: view.frame, startElevation: percent)
         fluidView.fillColor = UIColor.init(hex: 0x397ebe)
         fluidView.startAnimation()
+
         view.addSubview(fluidView)
+        // これは違う気がする
+        fluidView.addSubview(percentLabel)
     }
 }

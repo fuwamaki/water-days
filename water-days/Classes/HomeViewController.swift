e40b54390b9a723a783f77fe69a75da44a902f4e
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-03 15:28:54 +0900

    fix files

---
+++ water-days/Classes/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  water-days
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/02/02.
+//  Copyright © 2019年 sekimaki. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+

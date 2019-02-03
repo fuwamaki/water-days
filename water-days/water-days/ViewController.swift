dd1049a92b3a41e69d610993396c5bbb3ee95da1
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-03 14:30:21 +0900

    create water-days project

---
+++ water-days/water-days/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  water-days
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/02/02.
+//  Copyright © 2019年 sekimaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
